ce524abd71b4443fc1b697993d1cf3d48f4f16a4
xbacon14 <italo.galeano123@gmail.com>
2020-04-28 19:09:52 -0400

    empezado

---
+++ src/main/java/py/edu/facitec/PrimeraparcialItaloGolinApplication.java
@@ -0,0 +1,13 @@
+package py.edu.facitec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeraparcialItaloGolinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeraparcialItaloGolinApplication.class, args);
+	}
+
+}
